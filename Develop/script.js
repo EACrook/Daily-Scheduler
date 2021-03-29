2794e745f427d8058ff7f3ff9321f643623f9ad6
Emily Crook <Emily.A.Crook96@gmail.com>
2021-03-28 20:34:03 -0500

    time-blocks on page

---
+++ Develop/script.js
@@ -0,0 +1,15 @@
+var timeBlocks = document.querySelector("#time-block")
+
+// Clone the time-block entries
+function createTimeBlockEl() {
+$(timeBlocks).clone(true);
+}
+// Get date to display on page
+
+// Get notes sections on the page
+
+// Allow for clicked section to be edited 
+
+// Allow for save button to save the text
+
+createTimeBlockEl();
