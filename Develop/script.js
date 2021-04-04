e9c242542bb9f2ba93014b94ca016231250a7795
Emily Crook <Emily.A.Crook96@gmail.com>
2021-04-04 17:42:03 -0500

    fixed display of time/save btn

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,20 @@
+var today = moment();
+var tasks = {};
 var taskDescriptionEl = document.querySelector("description")
+
  
 // Allow for save button to save the text
 
 // Get date to display on page
 
 // Save to local server
+var saveTasks = function() {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+}
+
+var loadTasks = function () {
+    localStorage.getItem("tasks", tasks);
+    console.log(loadTasks);
+}
+
+displayDateTime();
