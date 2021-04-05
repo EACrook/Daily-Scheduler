941e79ded513af5f8e564f06b0957da28b6a4028
Emily Crook <Emily.A.Crook96@gmail.com>
2021-04-04 20:48:52 -0500

    attempt at saving tasks locally

--- Develop/script.js
+++ Develop/script.js
@@ -4,12 +4,7 @@ var tasks = {};
 var taskDescriptionEl = document.querySelector("description")
 
 // create task
-var createTaskEl = function(taskDataObj) {
-    var inputTaskEl = document.createElement("input");
-    inputTaskEl.className = "created-task col-10";
-    inputTaskEl.setAttribute("type", "text")
-    taskDescriptionEl.appendChild(displayTaskEl);
-}
+
 
  
 // Allow for save button to save the text
@@ -21,10 +16,55 @@ var saveTasks = function() {
     localStorage.setItem("tasks", JSON.stringify(tasks))
 }
 
+// Changing textarea to a p element
+$(".task-description").on("click", "p", function() {
+    var text = $(this).text().trim();
+    var textInput = $("<textarea>").addClass("task-control").val(text);
+    
+    $(this).replaceWith(textInput);
+    textInput.trigger("focus");
+});
+
+$(".task-description").on("blur", "textarea", function() {
+    var text = $(this).val().trim();
+
+    var status = $(this).closets(".task-description").attr("id");
+
+    tasks[status].text = text
+    saveTasks();
+
+    var taskP = $("<p>").class("description").text(text)
+
+    $(this).replaceWith(taskP);
+});
+
+// save button clicked
+$(".saveBtn").click(function() {
+    var 
+})
+
 var loadTasks = function () {
-    localStorage.getItem("tasks", tasks);
-    console.log(loadTasks);
+    tasks = JSON.parse(localStorage.getItem("tasks"));
+
+    if (!tasks) {
+        tasks = {
+            9: [],
+            10: [],
+            11: [],
+            12: [],
+            13: [],
+            14: [],
+            15: [],
+            16: [],
+            17: []
+        };
+    }
+
+    $.each(task, function(list, arr) {
+        arr.forEach(function(task) {
+            createTask(task.text, task.time);
+        });
+    })
 }
 
-createTaskEl();
-displayDateTime();
+saveTasks();
