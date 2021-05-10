c216ae05280b7514270f44a77f4c2d3a2cfef282
Emily Crook <Emily.A.Crook96@gmail.com>
2021-05-10 09:45:39 -0500

    README

--- Develop/script.js
+++ Develop/script.js
@@ -1,70 +1,95 @@
-var today = moment();
-$("currentDay").text(today.format("dddd, MMMM Do"));
-var tasks = {};
-var taskDescriptionEl = document.querySelector("description")
+var tasks = loadTasks();
+saveTasks()
 
-// create task
+// ...start of color for time...
+function colorTimeApt() {
+    // ... sets the moment JS time to the var currentTime
+    var currentTime = moment()
+    // ...sets currentDay to text of day, month, date, hour & min and am or pm...
+    $("#currentDay").text(currentTime.format("dddd, MMMM Do hh:mm a"));
+    
+    
+    currentHour = currentTime.hour()
 
-
- 
-// Allow for save button to save the text
-
-// Get date to display on page
+    $('.description').each(function () {
+        //...of the html elements with description class, take their IDs and split on hyphen to grab the 2nd element and set it to hourString
+        var hourString = $(this).attr("id").split('-')[1];
+        // sets the hourString as an integer to assign it to hourInt
+        var hourInt = parseInt(hourString);
+        
+        // if the number grabbed hourString/hourInt is less than the current time (currentHour) add the class past and remove the other two classes
+        if (hourInt < currentHour) {
+            $(this).removeClass("present")
+            $(this).removeClass("future")
+            $(this).addClass("past")
+        }
+        // if the int is equal to the current time, assign present class and remove other two classes 
+        else if (hourInt === currentHour) {
+            $(this).removeClass("past")
+            $(this).removeClass("future")
+            $(this).addClass("present")
+        }
+        // if the int is anything else, assign the future class and remove other two classes
+        else {
+            $(this).removeClass("past")
+            $(this).removeClass("present")
+            $(this).addClass("future")
+        }
+    })
+}
+//..call/run function
+colorTimeApt();
+//.. sets above function to rerun ever 5 seconds
+setInterval(colorTimeApt, 5 * 1000)
 
 // Save to local server
-var saveTasks = function() {
+function saveTasks() {
     localStorage.setItem("tasks", JSON.stringify(tasks))
 }
 
-// Changing textarea to a p element
-$(".task-description").on("click", "p", function() {
-    var text = $(this).text().trim();
-    var textInput = $("<textarea>").addClass("task-control").val(text);
-    
-    $(this).replaceWith(textInput);
-    textInput.trigger("focus");
-});
-
-$(".task-description").on("blur", "textarea", function() {
-    var text = $(this).val().trim();
-
-    var status = $(this).closets(".task-description").attr("id");
-
-    tasks[status].text = text
-    saveTasks();
-
-    var taskP = $("<p>").class("description").text(text)
-
-    $(this).replaceWith(taskP);
-});
-
 // save button clicked
-$(".saveBtn").click(function() {
-    var 
+$(".saveBtn").click(function () {
+    // of the button get the previous textarea
+    var valueOfWhatWasSaved = $(this).prev("textarea").val();
+    // of the button get the previous textarea id
+    var idOfTextArea = $(this).prev("textarea").attr("id")
+    // from the textarea id, split by hyphen to retrieve 2nd value which will be the # 
+    var time = idOfTextArea.split('-')[1]
+
+    // update value of tasks at the time to be the value of the text area
+    tasks[time] = valueOfWhatWasSaved
+
+    // call the save function
+    saveTasks()
 })
 
-var loadTasks = function () {
-    tasks = JSON.parse(localStorage.getItem("tasks"));
-
-    if (!tasks) {
-        tasks = {
-            9: [],
-            10: [],
-            11: [],
-            12: [],
-            13: [],
-            14: [],
-            15: [],
-            16: [],
-            17: []
+function loadTasks() {
+    //...if there is something in localStorage, get it and assign to the currentTasks var
+    var currentTasks = localStorage.getItem("tasks");
+    //..if nothing is saved to localStorage, return blank strings
+    if (currentTasks === null) {
+        return {
+            9: "",
+            10: "",
+            11: "",
+            12: "",
+            13: "",
+            14: "",
+            15: "",
+            16: "",
+            17: ""
         };
     }
+    //..set the value in current task as an object
+    currentTasks = JSON.parse(currentTasks);
+    // in means object of means array
+    for (var time in currentTasks) {
+        createTask(time, currentTasks[time]);
+    }
 
-    $.each(task, function(list, arr) {
-        arr.forEach(function(task) {
-            createTask(task.text, task.time);
-        });
-    })
+    return currentTasks;
 }
 
-saveTasks();
+function createTask(time, text) {
+    $(`#time-${time}`).val(text)
+}
--- README.md
+++ README.md
@@ -1,6 +1,5 @@
 # Work Day Scheduler Starter Code
+Work Day Scheduler allows you to keep track of your todo's on an hourly basis for a standard work day. 
 
-# Unfinished project
-## still not quite grasping saving to local API; going to attempt to redo project after doing more research and hopefully getting some additional help
-
-# Deployed application: https://eacrook.github.io/Daily-Scheduler/
+## Usage
+Clicking on the hour's text box will allow you to enter in your task. If the hour has passed, the line is red and will not let you edit. If the hour has not passed, the hour will be green and allow you to edit the task.
